5bcd58cfa97391ddf54de0911587677b0adfad69
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2025-11-11 14:42:53 +0200

    feat/added-Book-class-and-validator-for-author-title-rating

---
+++ Reading List.Domain/Validators/BookValidators.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Reading_List.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_List.Domain.Validators
+{
+    public class BookValidator : AbstractValidator<Book>
+    {
+        public BookValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required.");
+            RuleFor(x => x.Author).NotEmpty().WithMessage("Author is required.");
+            RuleFor(x => x.Rating).InclusiveBetween(0,5).When(x=> x.Rating.HasValue).WithMessage("Rating must be between 0 and 5.");
+
+        }
+    }
+}
