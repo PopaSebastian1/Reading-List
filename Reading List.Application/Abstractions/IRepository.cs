42637a4656ee1c7ee4aab19649d9a98fe0a8dbde
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2025-11-12 09:30:42 +0200

    feat/adding-IRepository-and-Result

---
+++ Reading List.Application/Abstractions/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Reading_List.Domain.Models;
+namespace Reading_List.Application.Abstractions
+{
+    public interface IRepository<T> where T : class
+    {
+
+        Task<Result<T>> AddAsync(T entity);
+
+        Task<Result<T>> UpdateAsync(T entity);
+
+        Task<Result<bool>> DeleteAsync(T entity);
+
+        Task<Result<T?>> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<Result<T>>> GetAllAsync();
+
+    }
+}
