8c11d44930ad89aa4deb75945f95a8ae6776aa7a
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2025-11-13 20:19:10 +0200

    refactor/split-import-export-functionalities

---
+++ Reading List.Application/Abstractions/IBookExportStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Reading_List.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_List.Application.Abstractions
+{
+    public interface IBookExportStrategy
+    {
+        IEnumerable<string> SupportedExtensions { get; }
+        Task<Result<bool>> ExportAsync(IEnumerable<Book> books, string filePath, CancellationToken ct = default);
+    }
+}
