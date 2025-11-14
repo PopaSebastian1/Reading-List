16a6a3c3409cf1b38f512097b51e65127ddfc959
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2025-11-14 15:37:59 +0200

    refactor/program-cs-with-DI-container

---
+++ Reading List.CLI/DIContainer.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Reading_List.Application.Abstractions;
+using Reading_List.Application.Commands;
+using Reading_List.Application.Services;
+using Reading_List.Domain.Models;
+using Reading_List.Domain.Validators;
+using Reading_List.Infrastructure.FileService;
+using Reading_List.Infrastructure.Repositories;
+
+namespace Reading_List.CLI
+{
+    public static class DIContainer
+    {
+        public static IServiceProvider Build()
+        {
+            var services = new ServiceCollection();
+
+            // Infrastructure / Repositories
+            services.AddSingleton<IRepository<Book>>(new InMemoryRepository<int, Book>(b => b.Id));
+            services.AddSingleton<IBookImportService, BookImportService>();
+            services.AddSingleton<IBookExportStrategy, JsonBookExportStrategy>();
+            services.AddSingleton<IBookExportStrategy, CsvBookExportStrategy>();
+            services.AddSingleton<IBookExportService, BookExportService>();
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+            // Application services & validators
+            services.AddSingleton<IBookService, BookService>();
+            services.AddSingleton<IValidator<Book>, BookValidator>();
+
+            // CLI commands
+            services.AddSingleton<ICommand, ImportCSVsCommand>();
+            services.AddSingleton<ICommand, ListAllCommand>();
+            services.AddSingleton<ICommand, GetTopRatedCommand>();
+            services.AddSingleton<ICommand, GetFinishedCommand>();
+            services.AddSingleton<ICommand, GetBooksByAuthor>();
+            services.AddSingleton<ICommand, MarkAsFinishedCommand>();
+            services.AddSingleton<ICommand, SetRatingCommand>();
+            services.AddSingleton<ICommand, ClearConsoleCommand>();
+            services.AddSingleton<ICommand, ExportBooksCommand>();
+            services.AddSingleton<ICommand, GetStatsCommand>();
+
+            // CLI Menu
+            services.AddSingleton<Menu>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
